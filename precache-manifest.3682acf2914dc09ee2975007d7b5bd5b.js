b85abe331830c3757f15f879752c5b30fc5ce0da
Will Cooper Jr <wcooperjr46@gmail.com>
2019-09-23 16:06:00 -0400

    Updates

---
+++ precache-manifest.3682acf2914dc09ee2975007d7b5bd5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac015f4d2daaf1976cf6d04a508cb776",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "bc35b8734ef324c59b03",
+    "url": "/todo-list/static/css/main.49d74c23.chunk.css"
+  },
+  {
+    "revision": "4aa074893c98980d2830",
+    "url": "/todo-list/static/js/2.12e8f2df.chunk.js"
+  },
+  {
+    "revision": "bc35b8734ef324c59b03",
+    "url": "/todo-list/static/js/main.c1c026bc.chunk.js"
+  },
+  {
+    "revision": "1c75938d14705f09f982",
+    "url": "/todo-list/static/js/runtime-main.1eda5f08.js"
+  }
+]);
